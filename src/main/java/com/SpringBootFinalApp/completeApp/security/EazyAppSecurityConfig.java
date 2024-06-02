cef1ec9308f85e648cd6d6b392655dbb79427f91
KRISHNADUTH <krishnaduth777@gmail.com>
2024-06-02 19:32:58 +0530

    Experiment with InmemmoryUserDetailsManager and JdbcUserDetailsManager

--- src/main/java/com/SpringBootFinalApp/completeApp/security/EazyAppSecurityConfig.java
+++ src/main/java/com/SpringBootFinalApp/completeApp/security/EazyAppSecurityConfig.java
@@ -1,20 +1,40 @@
 package com.SpringBootFinalApp.completeApp.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class EazyAppSecurityConfig {
     
+    // @Bean
+    // public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    // {
+    //     UserDetails john = User.builder().username("JohnDoe")
+    //             .password("11111").build();
+    //     return new InMemoryUserDetailsManager(john);
+    // }
+
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource)
     {
-        UserDetails john = User.builder().username("JohnDoe").password("{bcrypt}$2a$12$m.UWaEtURPNAE9TlAs8OLOtShWIQ8/tXjLw5jOgx6zywFpMQIAauy").build();
-        return new InMemoryUserDetailsManager(john);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("SELECT email, pwd, 1 as enabled from customer c where email=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("SELECT email, role from customer where email=?");
+        return userDetailsManager;
+        // return new JdbcUserDetailsManager(dataSource);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
